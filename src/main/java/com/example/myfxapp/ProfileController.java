0fcf4e3944dc1be0ec0897c22dcd2de0883515ba
d4ruh <rudabrandao@ruoso.com>
2023-06-27 00:00:01 -0300

    pagina de perfil conectada a db

--- src/main/java/com/example/myfxapp/ProfileController.java
+++ src/main/java/com/example/myfxapp/ProfileController.java
@@ -1,12 +1,57 @@
 package com.example.myfxapp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.fxml.Initializable;
 
-public class ProfileController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class ProfileController implements Initializable {
     @FXML Button logoutButton;
     @FXML Button voltarButton;
+
+    @FXML TextField nomeText;
+    @FXML TextField usernameText;
+    @FXML TextField sobrenomeText;
+    @FXML TextField cpfText;
+    @FXML TextField enderecoText;
+    @FXML TextField passwordText;
+    @FXML TextField emailText;
+    @FXML TextField telefoneText;
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+        DatabaseHandler connect = new DatabaseHandler();
+        Connection conDB = connect.getConnection();
+
+        String getData = "select * from registro_vendedor where username='" + Data.userLogedIn + "';";
+
+        try {
+            Statement stmt = conDB.createStatement();
+            ResultSet rs = stmt.executeQuery(getData);
+
+            while (rs.next()) {
+                usernameText.setText(rs.getString(1));
+                passwordText.setText(rs.getString(2));
+                nomeText.setText(rs.getString(3));
+                sobrenomeText.setText(rs.getString(4));
+                emailText.setText(rs.getString(5));
+                telefoneText.setText(rs.getString(6));
+                cpfText.setText(rs.getString(7));
+                enderecoText.setText(rs.getString(8));
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     @FXML
     protected void onLogoutButtonClick() {
