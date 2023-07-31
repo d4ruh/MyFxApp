2bf60927d9efe94b6adac05727dda8cd535dc461
vito9 <vito9909hugo@gmail.com>
2023-07-31 15:43:44 -0300

    Mudei o nome das imagens e coloquei um arquivo css. com as imagens corretas agora. E fxml corretos.

--- src/main/java/com/example/myfxapp/HelloApplication.java
+++ src/main/java/com/example/myfxapp/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.myfxapp;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,10 +16,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        Scene scene = new Scene(root);
+
         Image icon= new Image(new File("src/main/java/com/example/myfxapp/imagens/icon/Logo.png").toURI().toString());
         stage.setTitle("WoodPecker Furniture - login");
         stage.getIcons().add(icon);
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/styles/Styles.css").toExternalForm());
 
         stage.setScene(scene);
         stage.show();
